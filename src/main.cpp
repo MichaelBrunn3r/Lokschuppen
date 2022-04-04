3f851f0a5814fcd89b861be591ab3349fb20e5e7
MichaelBrunn3r <MichaelBrunn3r@gmail.com>
2022-04-04 16:31:05 +0200

    build: Added .clang-format

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <Arduino.h>
-#include <ESPAsyncWebServer.h>
-#include <WiFi.h>
 #include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
 #include <SPIFFS.h>
+#include <WiFi.h>
 
 #define USE_INTRANET true
 #define DEBUG true
@@ -13,52 +13,57 @@
 const char* hostname = "lokschuppen";
 AsyncWebServer server(80);
 
-void handleRoot(AsyncWebServerRequest *request) {
-  if(!SPIFFS.begin()){
-    logln("Error: Cannot mount filesystem");
-    return;
-  }
+void handleRoot(AsyncWebServerRequest* request) {
+    if (!SPIFFS.begin()) {
+        logln("Error: Cannot mount filesystem");
+        return;
+    }
 
-  request->send(SPIFFS, "/index.html", "text/html");
+    request->send(SPIFFS, "/index.html", "text/html");
 }
 
 bool connectToAccessPoint() {
-  #if USE_INTRANET
+#if USE_INTRANET
     log("Connecting to WiFi ... ");
     WiFi.mode(WIFI_STA);
+    WiFi.config(INADDR_NONE, INADDR_NONE, INADDR_NONE, INADDR_NONE);
     WiFi.setHostname(hostname);
     WiFi.begin(WiFi_SSID, WiFi_PASSWORD);
     if (WiFi.waitForConnectResult() != WL_CONNECTED) {
-      logln("Failed!");
-      return false;
+        logln("Failed!");
+        return false;
     }
     logln("OK");
-    log("IP Address: "); logln(WiFi.localIP());
-  #else
+    log("IP Address: ");
+    logln(WiFi.localIP());
+#else
     logln("Creating Access Point ... ");
     WiFi.softAP(AP_SSID, AP_PASSWORD);
     logln("OK");
-    log("IP Address: "); logln(WiFi.softAPIP());
-  #endif
+    log("IP Address: ");
+    logln(WiFi.softAPIP());
+#endif
 
-  return true;
+    return true;
 }
 
 void initServer() {
-  server.on("/", HTTP_GET, handleRoot);
-  server.serveStatic("/", SPIFFS, "/");
-  server.begin();
+    server.on("/", HTTP_GET, handleRoot);
+    server.serveStatic("/", SPIFFS, "/");
+    server.begin();
 }
 
 void setup() {
-  // Init serial connection
-  Serial.begin(115200);
-  while (!Serial && !Serial.available()) {}
-  Serial.println();
+    // Init serial connection
+    Serial.begin(115200);
+    while (!Serial && !Serial.available()) {
+    }
+    Serial.println();
 
-  // Init server
-  if(!connectToAccessPoint()) return;
-  initServer();
+    // Init server
+    if (!connectToAccessPoint())
+        return;
+    initServer();
 }
 
 void loop() {}
